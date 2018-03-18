a47d278a3f87c3743e19980cf4cb0a918c6ff271
CarrotShaver <rossim@hawaii.edu>
2018-03-17 20:11:21 -1000

    Worked on web pages added jinja and FlaskForm modules

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # FlaskWebApps
+
+### Install flask-wtf
+- Any OS: `pip install flask-wtf`
+
+### Start virtual environment
+- Linux: `source env/bin/activate`
+- Windows: Good luck dude
+
+### Create requirements.txt
+- Note: Run everytime a new module is installed
+- Any OS: `pip freeze > requirements.txt`
+
+### Create flask run environment variable
+- Linux: `export FLASK_APP=server.py`
+- Windows: `set FLASK_APP=server.py`
+
+### Turn on Flask Debugger
+- Linux: `export FLASK_APP=server.py`
+- Windows: `set FLASK_APP=server.py`
+
+### Start Flask server
+- Any OS: `flask run`
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,21 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
+from app.forms import LoginForm
 
 @app.route("/")
 @app.route("/index", methods=["GET", "POST"])
 def index():
+    data = request.form
+    print("form data", data)
     user = {
-        "username" : "SomeUsername", 
-        "age" : "27" }
+        "username" : ""}
     return render_template("index.html", title="Home", user=user)
 
 @app.route("/register")
 def register():
     return render_template("register.html", title="Create New User Login")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Sign In", form=form)
