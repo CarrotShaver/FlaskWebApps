0515e2f726d0e4cba3979e1074621e8478667ba8
CarrotShaver <rossim@hawaii.edu>
2018-03-31 11:51:17 -1000

    updated routes

--- app/routes.py
+++ app/routes.py
@@ -52,9 +52,6 @@ def register():
         return redirect(url_for("index"))
     form = Registration()
 
-    data = (form.data)
-    print(data)
-
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
